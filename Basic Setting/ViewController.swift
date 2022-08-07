be3ec571f41a9efa5bc19389851ee6b7ce1c28ee
Albina <olifirenko.albina@gmail.com>
2022-08-07 23:04:55 +0300

    Create change view by slider with showing some number

--- Basic Setting/ViewController.swift
+++ Basic Setting/ViewController.swift
@@ -8,12 +8,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redColor: UISlider!
+    @IBOutlet var greenColor: UISlider!
+    @IBOutlet var blueColor: UISlider!
+    
+    @IBOutlet var numberChangeRed: UILabel!
+    @IBOutlet var numberChangeGreen: UILabel!
+    @IBOutlet var numberChangeBlue: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        descriptionColorView()
     }
-
+    
+    @IBAction func moveSlider() {
+        numberChangeRed.text = String(redColor.value)
+        numberChangeGreen.text = String(greenColor.value)
+        numberChangeBlue.text = String(blueColor.value)
+        
+       /* view.backgroundColor = view.backgroundColor?.withAlphaComponent(CGFloat(redColor.value, greenColor.value, blueColor.value))
+        */
+       /* view.backgroundColor = UIColor(red: , green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        */
+    }
+    
+    
+    
+    //MARK: - descriptions objects
+    private func descriptionColorView() {
+        mainView.layer.cornerRadius = 10
+    }
+    
+    private func setupSlider() {
+        
+        redColor.value = 50
+        redColor.maximumValue = 1
+        redColor.minimumValue = 0
+        
+        greenColor.value = 50
+        greenColor.maximumValue = 1
+        greenColor.minimumValue = 0
+        
+        blueColor.value = 50
+        blueColor.maximumValue = 1
+        blueColor.minimumValue = 0
+        
+    }
+    
+    
+    class Color {
+        private var red:CGFloat
+        private var green:CGFloat
+        private var blue:CGFloat
+        
+        init(red:Float, green:Float, blue:Float) {
+            self.red = CGFloat(red)
+            self.green = CGFloat(green)
+            self.blue = CGFloat(blue)
+        }
+        
+        private func colorView() -> UIColor {
+            let color = UIColor(red: red/100, green: green/100, blue: blue/100, alpha: 1)
+            return color
+        }
+        
+    }
 
 }
 
